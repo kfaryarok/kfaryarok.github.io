ac2177cc965945de32c7c6ff1b9f3b6e28037586
Rotem <rotem@faceco.de>
2020-11-22 11:20:02 +0200

    Update

--- js/content.js
+++ js/content.js
@@ -1,4 +1,8 @@
-const events = JSON.parse("{\"updated\":\"2020-11-21\",\"events\":[{\"date\":\"22/11/2020\",\"type\":\"activity\",\"name\":\"סדנאות הכנה לצו ראשון\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"25/11/2020\",\"type\":\"test\",\"name\":\"מתמטיקה\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"29/11/2020\",\"type\":\"test\",\"name\":\"אלקטרוניקה\",\"classes\":[2,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"01/12/2020\",\"type\":\"test\",\"name\":\"ספרות\",\"classes\":[1,2,4,5,6,7,9,10,13]},{\"date\":\"01/12/2020\",\"type\":\"test\",\"name\":\"רוסית\",\"classes\":[11]},{\"date\":\"03/12/2020\",\"type\":\"test\",\"name\":\"ספרות\",\"classes\":[8,12]},{\"date\":\"08/12/2020\",\"type\":\"test\",\"name\":\"היסטוריה\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"10/12/2020\",\"type\":\"test\",\"name\":\"אנגלית\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"11-19/12/2020\",\"type\":\"activity\",\"name\":\"חופשת חנוכה\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"23/12/2020\",\"type\":\"matkonet\",\"name\":\"אנגלית\",\"classes\":[4,5]},{\"date\":\"28/12/2020\",\"type\":\"matkonet\",\"name\":\"היסטוריה\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]},{\"date\":\"10/02/2021\",\"type\":\"bagrut\",\"name\":\"אנגלית\",\"classes\":[4,5]},{\"date\":\"16/02/2021\",\"type\":\"bagrut\",\"name\":\"היסטוריה\",\"classes\":[1,2,3,4,5,6,7,8,9,10,11,12,13]}]}");
+const url = 'https://kfaryarok.github.io/data/events.json';
+var events;
+fetch(url)
+  .then(response => response.text())
+  .then(result => events = JSON.parse(result));
 
 const checkboxes = [
   "physics", "chemistry", "biology", "philosophy", "arabic", "environment", "social",
